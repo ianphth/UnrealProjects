4b06142c3adfae0c460941605f6efefca95626bd
ianphth <ianphth@gmail.com>
2018-01-21 03:24:00 +0800

    Project name changed

--- PWShowcase/Source/PWShowcase/MusicSync.cpp
+++ PWShowcase/Source/PWShowcase/MusicSync.cpp
@@ -2,33 +2,146 @@
 
 #include "MusicSync.h"
 
+static class AMusicSync* CurrentSync;
+static TArray<class AMusicSync*> SyncList;
 
-// Sets default values for this component's properties
-UMusicSync::UMusicSync()
+// Sets default values
+AMusicSync::AMusicSync()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
-	// ...
 }
 
-
-// Called when the game starts
-void UMusicSync::BeginPlay()
+// Called when the game starts or when spawned
+void AMusicSync::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	CurrentSync = this;
 }
-
 
 // Called every frame
-void UMusicSync::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void AMusicSync::Tick(float DeltaTime)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::Tick(DeltaTime);
 
-	// ...
 }
 
+void AMusicSync::SectionValidate(FMusicSection inputSection, int startTimeSample)
+{
+	inputSection.StartTimeSamples = startTimeSample;
+}
+
+void AMusicSync::TimingCopy(FMusicTiming inputTiming, FMusicTiming copy)
+{
+	inputTiming = copy;
+}
+
+void AMusicSync::TimingSet(FMusicTiming inputTiming, int bar, int beat = 0, int unit = 0)
+{
+	inputTiming.Bar = bar;
+	inputTiming.Unit = unit;
+	inputTiming.Beat = beat;
+}
+
+int AMusicSync::GetMusicalTime(FMusicTiming inputTiming, FMusicSection section)
+{
+	return inputTiming.Bar * section.UnitPerBar + inputTiming.Beat * section.UnitPerBeat + inputTiming.Unit;
+}
+
+void AMusicSync::TimingFix(FMusicTiming inputTiming, FMusicSection section)
+{
+	int totalUnit = inputTiming.Bar * section.UnitPerBar + inputTiming.Beat * section.UnitPerBeat + inputTiming.Unit;
+	inputTiming.Bar = totalUnit / section.UnitPerBar;
+	inputTiming.Beat = (totalUnit - inputTiming.Bar * section.UnitPerBar) / section.UnitPerBeat;
+	inputTiming.Unit = (totalUnit - inputTiming.Bar * section.UnitPerBar - inputTiming.Beat * section.UnitPerBeat);
+}
+
+void AMusicSync::UnitIncrement(FMusicTiming inputTiming, FMusicSection section)
+{
+	inputTiming.Unit++;
+	TimingFix(inputTiming, section);
+}
+
+void AMusicSync::UnitDecrement(FMusicTiming inputTiming, FMusicSection section)
+{
+	inputTiming.Unit--;
+	TimingFix(inputTiming, section);
+}
+
+void AMusicSync::BeatIncrement(FMusicTiming inputTiming, FMusicSection section)
+{
+	inputTiming.Beat++;
+	TimingFix(inputTiming, section);
+}
+
+void AMusicSync::TimingAdd(FMusicTiming inputTiming, FMusicTiming addTiming, FMusicSection section)
+{
+	inputTiming.Bar += addTiming.Bar;
+	inputTiming.Beat += addTiming.Beat;
+	inputTiming.Unit += addTiming.Unit;
+	TimingFix(inputTiming, section);
+}
+
+void AMusicSync::TimingSub(FMusicTiming inputTiming, FMusicTiming subTiming, FMusicSection section)
+{
+	inputTiming.Bar -= subTiming.Bar;
+	inputTiming.Beat -= subTiming.Beat;
+	inputTiming.Unit -= subTiming.Unit;
+	TimingFix(inputTiming, section);
+}
+
+bool AMusicSync::TimingEquals(FMusicTiming t1, FMusicTiming t2)
+{
+	return (t1.Bar == t2.Bar && t1.Beat == t2.Beat && t1.Unit == t2.Unit);
+}
+
+int AMusicSync::TimingCompare(FMusicTiming t1, FMusicTiming t2)
+{
+	if (TimingEquals(t1, t2))
+	{
+		return 0;
+	}
+	else if (t1 > t2)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+double AMusicSync::Lag_()
+{
+	if (isFormerHalf_)
+	{
+		return timeSecFromJust_;
+	}
+	else
+	{
+		return timeSecFromJust_ - musicTimeUnit_;
+	}
+}
+
+double AMusicSync::LagAbs_()
+{
+	if (isFormerHalf_)
+	{
+		return timeSecFromJust_;
+	}
+	else
+	{
+		return musicTimeUnit_ - timeSecFromJust_;
+	}
+}
+
+double AMusicSync::Lag()
+{
+	return CurrentSync->Lag_();
+}
+
+double AMusicSync::LagUnit_()
+{
+	return Lag() / musicTimeUnit_;
+}
--- PWShowcase/Source/PWShowcase/MusicSync.h
+++ PWShowcase/Source/PWShowcase/MusicSync.h
@@ -3,27 +3,200 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
 #include "MusicSync.generated.h"
 
+UENUM(BlueprintType)
+enum class ESectionType : uint8
+{
+	Undefined,
+	Loop,
+	Through,
+	End
+};
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class PWSHOWCASE_API UMusicSync : public UActorComponent
+UENUM(BlueprintType)
+enum class ESyncType : uint8
+{
+	NextBeat,
+	Next2Beat,
+	NextBar,
+	Next2Bar,
+	Next4Bar,
+	Next8Bar,
+	SectionEnd
+};
+
+USTRUCT(BlueprintType)
+struct FMusicSection
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		FString SectionName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int UnitPerBeat;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int UnitPerBar;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		float Tempo;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int StartBar;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int StartTimeSamples;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		ESectionType LoopType;
+
+	FMusicSection()
+	{
+		SectionName = "None";
+		UnitPerBeat = 4;
+		UnitPerBar = 16;
+		Tempo = 120.0;
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FMusicTiming
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int Beat;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int Bar;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int Unit;
+	
+	bool operator >(const FMusicTiming t2) 
+	{ 
+		return Bar > t2.Bar || (Bar == t2.Bar && Beat > t2.Beat) || (Bar == t2.Bar && Beat == t2.Beat && Unit > t2.Unit); 
+	}
+	bool operator ==(const FMusicTiming t2)
+	{
+		return (Bar == t2.Bar && Beat == t2.Beat && Unit == t2.Unit);
+	}
+	bool operator <(const FMusicTiming t2) 
+	{ 
+		return !(*this > t2) && !(*this == t2);
+	}
+	bool operator <=(const FMusicTiming t2) 
+	{ 
+		return !(*this > t2); 
+	}
+	bool operator >=(const FMusicTiming t2) 
+	{ 
+		return (*this > t2) || (*this == t2);
+	}
+
+	FMusicTiming(int bar = 0, int beat = 0, int unit = 0)
+	{
+		Beat = beat;
+		Bar = bar;
+		Unit = unit;
+	}
+};
+
+UCLASS()
+class PWSHOWCASE_API AMusicSync : public AActor
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
-	UMusicSync();
+	// Sets default values for this actor's properties
+	AMusicSync();
+#pragma region Params
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		TArray<FMusicSection> MusicSections;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		class USoundCue* MusicSource;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		int EntryPointSample;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MusicSync")
+		bool CreateSectionClips;
+#pragma endregion
 
+#pragma region Properties
+	static double Lag() ;
+#pragma endregion
+private:
+#pragma region Params
+	FMusicTiming just_;
+	FMusicTiming near_;
+	bool isJustChanged_;
+	bool isNearChanged_;
+	bool isFormerHalf_;
+	double timeSecFromJust_;
+	int numRepeat_;
+	double musicTimeUnit_;
+	class USoundCue* musicSource_;
+	int sectionIndex_;
+	int currentSample_;
+	int samplingRate_;
+	int samplesPerUnit_;
+	int samplesPerBeat_;
+	int samplesPerBar_;
+	int samplesInLoop_;
+	FMusicTiming oldNear_, oldJust_;
+	int numLoopBar_ = -1;
+	TArray<class USoundCue*> quantizedCue_;
+	const float PITCH_UNIT = FGenericPlatformMath::Pow(2.0f, 1.0f / 12.0f);
+	TArray<class USoundCue*> sectionSources_;
+	bool isTransitioning_ = false;
+	FMusicTiming transitionTiming_ = FMusicTiming(0, 0, 0);
+#pragma endregion
+
+#pragma region Properties
+	double Lag_();
+	double LagAbs_();
+	double LagUnit_();
+	double LagUnit_();
+	float MusicalTime_();
+	float MusicalTimeBar_();
+	float AudioTimeSec_();
+	int TimeSamples_();
+	int SectionCount_();
+	int UnitPerBar_();
+	int UnitPerBeat_();
+	bool IsPlaying_();
+	FMusicSection CurrentSection_();
+#pragma endregion
 protected:
-	// Called when the game starts
+	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadWrite, Category = "MusicSync")
+		UAudioComponent* MusicPlayer;
 
 public:	
 	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void Tick(float DeltaTime) override;
+#pragma region Functions
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void SectionValidate(FMusicSection inputSection, int startTimeSample);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void TimingCopy(FMusicTiming inputTiming, FMusicTiming copy);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void TimingSet(FMusicTiming inputTiming, int bar, int beat, int unit);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void TimingFix(FMusicTiming inputTiming, FMusicSection section);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void UnitIncrement(FMusicTiming inputTiming, FMusicSection section);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void UnitDecrement(FMusicTiming inputTiming, FMusicSection section);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void BeatIncrement(FMusicTiming inputTiming, FMusicSection section);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void TimingAdd(FMusicTiming inputTiming, FMusicTiming addTiming, FMusicSection section);
+	UFUNCTION(BlueprintCallable, Category = "MusicSync")
+		void TimingSub(FMusicTiming inputTiming, FMusicTiming subTiming, FMusicSection section);
+	UFUNCTION(BlueprintPure, Category = "MusicSync")
+		bool TimingEquals(FMusicTiming t1, FMusicTiming t2);
+	UFUNCTION(BlueprintPure, Category = "MusicSync")
+		int TimingCompare(FMusicTiming t1, FMusicTiming t2);
+	UFUNCTION(BlueprintPure, Category = "MusicSync")
+		int GetMusicalTime(FMusicTiming inputTiming, FMusicSection section);
+#pragma endregion
+private:
 
-		
-	
 };
